70818495d329265785580ed167cc744270933631
AndrewVasutenko <vasutenko@gmail.com>
2022-09-13 00:29:42 +0200

    Fixed minor problems

--- homework.py
+++ homework.py
@@ -1,3 +1,7 @@
+#Сделайте функцию, которая будет печатать читаемое имя переданной ей функции и значений аргументов.
+#Вызовите ее внутри функций, описанных ниже
+#Подсказка: Имя функции можно получить с помощью func.__name__
+
 def print_name_arguments(func_name, *args):
     func_name = func_name.__name__.replace("_", " ").capitalize()
     print(func_name, *args)
@@ -9,7 +13,7 @@ def go_to_companyname_homepage(page_url):
     print_name_arguments(go_to_companyname_homepage , page_url)
 
 def find_registration_button_on_login_page(page_url, button_text):
-    print(find_registration_button_on_login_page , page_url,button_text)
+    print_name_arguments(find_registration_button_on_login_page , page_url, button_text)
 
 open_browser("Chrome")
 go_to_companyname_homepage("https://www.google.com")
